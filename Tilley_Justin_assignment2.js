78df841c19c57b0a33fc2097af8100e6cdc9f1c6
JTilley24 <justin.tilley@me.com>
2012-09-06 13:10:44 -0400

    Updated boolean function in javascript file.

--- Tilley_Justin_assignment2.js
+++ Tilley_Justin_assignment2.js
@@ -1,42 +1,58 @@
 //variables
 var name = "Mike";
 var jumpName = ["small", "big", "double", "triple"];
-var jumpNumber = 1;
+
 //procedure
 
 //boolean
 
+var tire = function(tireSpin, tireCatch) {
+			var tireWipeout = false,
+				tireImpact 
+			if ((tireSpin && tireCatch) || !tireWipeout) {
+				tireImpact = true
+			}
+			else {
+				tireImpact = false
+			};
+			return tireImpact;
+};
+
+var tireStatus = tire(true, true)
+console.log( "It is " + tireStatus + " that " + name + " gains control of the bike again.")
 
 //array
 
-var jump = function() {
+var jump = function(jumpNumber, size) {
 	var feet = [5, 10, 15,]
 	for (jumpHeight= 0; jumpHeight < feet.length; jumpHeight++) {
-		console.log(name + " hits the " + jumpName[jumpNumber] + " jump and goes " + feet[jumpHeight] + " feet.")
+		console.log(name + " hits the " + jumpName[jumpNumber] + " jump and goes " + 
+			feet[jumpHeight] + size[1])
 		};
 	var height = jumpNumber + feet
 	return height;
-
-
 };
-jump()
+
+jump(1, ['"', "'"])
+
 //number
 
 var gear = function(throttle) {
 		var speed
-		var jumpSpeed = jumpNumber * 2
+		var jumpSpeed = jumpName.length * 1
 		while(jumpSpeed > 0) { 
 		console.log(name + " can make the jump!");	jumpSpeed--
 		};
-		speed = throttle * jumpNumber
+		speed = throttle * jumpName.length
 		return speed
 };
+
 var bikeSpeed = gear(5)
 console.log(name + " makes the jump at " + bikeSpeed + " mph!")
 
 //string
 
-var tire = function() {
+var mike = function() {
 	var thought = "You can do this " + name;
 	
 	return thought
