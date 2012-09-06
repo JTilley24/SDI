58cb4df3c70b17a57e1b8d2bf1c02b6b8c3bbb12
Justin Tilley <Tilley@Justins-MacBook-Pro.local>
2012-09-05 23:32:57 -0400

    Updated javescript and corrected html file.

--- Tilley_Justin_assignment2.js
+++ Tilley_Justin_assignment2.js
@@ -1,2 +1,43 @@
-alert("JavaScript works!");
-console.log( "GitHub" )
+//variables
+var name = "Mike";
+var jumpName = ["small", "big", "double", "triple"];
+var jumpNumber = 1;
+//procedure
+
+//boolean
+
+
+//array
+
+var jump = function() {
+	var feet = [5, 10, 15,]
+	for (jumpHeight= 0; jumpHeight < feet.length; jumpHeight++) {
+		console.log(name + " hits the " + jumpName[jumpNumber] + " jump and goes " + feet[jumpHeight] + " feet.")
+		};
+	var height = jumpNumber + feet
+	return height;
+
+
+};
+jump()
+//number
+
+var gear = function(throttle) {
+		var speed
+		var jumpSpeed = jumpNumber * 2
+		while(jumpSpeed > 0) { 
+		console.log(name + " can make the jump!");	jumpSpeed--
+		};
+		speed = throttle * jumpNumber
+		return speed
+};
+var bikeSpeed = gear(5)
+console.log(name + " makes the jump at " + bikeSpeed + " mph!")
+
+//string
+
+var tire = function() {
+	var thought = "You can do this " + name;
+	
+	return thought
+};
