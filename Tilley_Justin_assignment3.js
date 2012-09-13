4f06e08cdeb8f580b63e0ebebe64caf532f20b7b
JTilley24 <justin.tilley@me.com>
2012-09-12 21:28:01 -0400

    Added json and created object

--- Tilley_Justin_assignment3.js
+++ Tilley_Justin_assignment3.js
@@ -1,1 +1,30 @@
-alert("JavaScript works!");
+var name = "Mike"
+var bike = {
+	"brand": ["Yamaha ", 
+			"Kawasaki ",
+			"Honda ",
+			"Suzuki "
+	],
+	"engineSize": 250,
+	"type": "dirt bike",
+	"stroke": {
+	 "twoStroke": "two stroke",
+	 "fourStroke": "four stroke"
+	},
+	
+	
+
+};
+
+
+console.log(name + " has a " + bike.brand[1] + bike.type + ".");
+console.log ("It has a " + bike.engineSize + "cc that is a " + bike.stroke.twoStroke + ".");
+var handleData = function(json) {
+		for(var i = 0; i < json.jumps.length; i++){
+		var jump = json.jumps[i]
+		console.log("The " + jump.name + " is " + jump.size + " feet.")
+		};
+
+};
+
+handleData(json2);
---
+++ json.js
@@ -0,0 +1,20 @@
+var json2 = {
+	"jumps": [
+		{
+			"name": "small",
+			"size": 5
+		},
+		{
+			"name": "big",
+			"size": 10
+		},
+		{
+			"name": "double",
+			"size": 15
+		},
+		{
+			"name": "triple",
+			"size": 20
+		}		
+	]
+};
